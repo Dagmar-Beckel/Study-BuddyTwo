25d55f721d69d19fbc5398bb07f7070211d65ff4
Dagmar-Beckel <beckeldagmar@gmail.com>
2023-07-19 10:40:24 -0500

    Almost Completed Bottom Action Sheet

--- Study BuddyTwo/View/Home.swift
+++ Study BuddyTwo/View/Home.swift
@@ -98,8 +98,121 @@ struct Home: View {
                 }
             }
             .padding()
+            .overlay(content:{
+                ZStack{
+                    Color.black
+                        .opacity(pomodoroModel.addNewTimer ? 0.25 : 0 )
+                        .onTapGesture{
+                            pomodoroModel.hour = 0
+                            pomodoroModel.minutes = 0
+                            pomodoroModel.seconds = 0
+                            pomodoroModel.addNewTimer = false
+                        }
+                    NewTimerView()
+                        .frame(maxHeight: .infinity, alignment: .bottom)
+                        .offset(y: pomodoroModel.addNewTimer ? 0 : 400)
+                }
+                .animation(.easeInOut, value: pomodoroModel.addNewTimer)
+            })
         }
-        
+    }
+    //MARK: New Timer Bottom Sheet
+    @ViewBuilder
+    func NewTimerView()->some View{
+        VStack(spacing: 15){
+            Text("Add New Timer")
+                .font(.title2.bold())
+                .foregroundColor(.black)
+                .padding(.top,10)
+            HStack(spacing:15){
+                Text("\(pomodoroModel.hour) hr")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black.opacity(0.2))
+                    .padding(.horizontal,20)
+                    .padding(.vertical,12)
+                    .background{
+                        Capsule()
+                            .fill(.white.opacity(0.2))
+                    }
+                    .contextMenu{
+                        ContextMenuOptions(maxValue: 12, hint: "hr"){value in
+                            pomodoroModel.hour = value
+                        }
+                        
+                    }
+                
+                Text("\(pomodoroModel.minutes) min")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black.opacity(0.2))
+                    .padding(.horizontal,20)
+                    .padding(.vertical,12)
+                    .background{
+                        Capsule()
+                            .fill(.white.opacity(0.2))
+                    }
+                    .contextMenu{
+                        ContextMenuOptions(maxValue: 60, hint: "min"){value in
+                            pomodoroModel.minutes = value
+                        }
+                        
+                    }
+                
+                Text("\(pomodoroModel.seconds) sec")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black.opacity(0.2))
+                    .padding(.horizontal,20)
+                    .padding(.vertical,12)
+                    .background{
+                        Capsule()
+                            .fill(.white.opacity(0.2))
+                    }
+                    .contextMenu{
+                        ContextMenuOptions(maxValue: 60, hint: "sec"){value in
+                            pomodoroModel.seconds = value
+                        }
+                        
+                    }
+            }
+            .padding(.top,20)
+            
+            Button{
+                
+            }label:{
+                Text("Save")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .padding(.vertical)
+                    .padding(.horizontal,100)
+                    .background{
+                        Capsule()
+                            .fill(Color("PastelBlueOne"))
+                    }
+            }
+            .disabled(pomodoroModel.seconds == 0)
+            .opacity(pomodoroModel.seconds == 0 ? 0.5 : 1)
+            .padding(.top)
+            
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background{
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(Color("PastelGreenDark"))
+                .ignoresSafeArea()
+        }
+    }
+    //MARK: Reusable Context Menu Options
+    @ViewBuilder
+    func ContextMenuOptions(maxValue: Int,hint: String,onClick: @escaping (Int)->())-> some View{
+        ForEach(0...maxValue,id: \.self){value in
+            Button("\(value) \(hint)"){
+                onClick(value)
+            }
+        }
     }
 }
 
